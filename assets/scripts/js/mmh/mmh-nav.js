e1cc039c91dda305596fe86a33f8396d21817187
pawsinsocks <max@tsauer.de>
2020-03-30 01:00:07 +0200

    Added files via upload

---
+++ assets/scripts/js/mmh/mmh-nav.js
@@ -0,0 +1,30 @@
+function menuFadeIn() {
+  document.getElementById('mmh-menu').style.width = '100%';
+}
+
+function menuFadeOut() {
+  document.getElementById('mmh-menu').style.width = '0%';
+}
+
+
+// scrollers
+
+function scrollTop() {
+  document.getElementById('landing').scrollIntoView();
+}
+
+function scrollWhy() {
+  document.getElementById('why').scrollIntoView();
+}
+
+function scrollReviews() {
+  document.getElementById('users').scrollIntoView();
+}
+
+function scrollCEpisode() {
+  document.getElementById('current_episode').scrollIntoView();
+}
+
+function scrollArchive() {
+  document.getElementById('lib').scrollIntoView();
+}
